2fe4f7e298a13bcdab5cb0068513a027c6059530
JakapanNewpila <102730156+JakapanNewpila@users.noreply.github.com>
2022-05-23 23:59:27 +0700

    Add files via upload

---
+++ neweb/script.js
@@ -0,0 +1,21 @@
+// function Changebg(){
+//     document.body.style.backgroundColor="red";
+//     document.body.style.backgroundColor = "black";
+// }
+// // function Changebg();
+
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    });
